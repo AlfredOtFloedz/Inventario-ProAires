18d819b10e660f083e015665a582908625d64e44
AlfrdOtFloedz <alfre_otofh@hotmail.com>
2023-11-21 00:08:45 -0600

    change in hosts: ['*']

--- inventario/settings.py
+++ inventario/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(#4g2j&rcs8oql$v!bkb7v=tt3v4f3wz4s7ldd=_2fce_)h(%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
