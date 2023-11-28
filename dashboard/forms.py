54f5e1e1c1438ec91b86ee39a63da4adfb35896d
AlfrdOtFloedz <alfre_otofh@hotmail.com>
2023-11-28 16:16:56 -0600

    Change in order form, look through code

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . models import Producto, Order, Customer
+from django.forms import ValidationError
 
 class ProductoForm(forms.ModelForm):
     
@@ -16,11 +17,27 @@ class ProductoForm(forms.ModelForm):
         }
     
 class OrderForm(forms.ModelForm):
+    product_code = forms.CharField(label='Código del Producto')
+
     class Meta:
         model = Order
-        fields = ['product', 'order_quantity', 'customer']
+        fields = ['product_code', 'order_quantity', 'customer']
         labels = {
-        'product': 'Producto',
-        'order_quantity': 'Cantidad',
-        'customer': 'Cliente',
+            'order_quantity': 'Cantidad',
+            'customer': 'Cliente',
         }
+
+    def clean_product_code(self):
+        product_code = self.cleaned_data['product_code']
+        products = Producto.objects.filter(code=product_code)
+
+        if not products.exists():
+            raise ValidationError('Producto no encontrado con el código proporcionado')
+        elif products.count() > 1:
+            raise ValidationError('Múltiples productos encontrados con el código proporcionado')
+        
+        return products.first()
+
+    def save(self, commit=True):
+        self.instance.product = self.cleaned_data['product_code']
+        return super().save(commit)
